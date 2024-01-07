c42a9255180136835da09b3c3cdafb65c1011b7b
Felipe Goncalves <felipe.cleia05@gmail.com>
2024-01-07 15:31:22 -0300

    inserido placeholders nos inputs e botão limpar

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,6 @@
-document.getElementById('calculate').addEventListener('click', function(){
+const calculate = document.getElementById('calculate') 
+
+calculate.addEventListener('click', function(){
   const value = document.getElementById('value').value;
   const fee = (document.getElementById('fee').value) / 100;
   const time = document.getElementById('time').value;
